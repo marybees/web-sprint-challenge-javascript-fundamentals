d23368c5eb4207071431d0fe1767ce9d9e315914
Mary Baker <meb601@gmail.com>
2020-07-24 11:46:11 -0400

    Correct Request 3

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -47,10 +47,8 @@ console.log(lowCaseAnimalNames);
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-const lowPopulationAnimals = [];
-zooAnimals.filter(function(animal) {
-  let endangered = animal.population < 5;
-  return lowPopulationAnimals.push(endangered);
+const lowPopulationAnimals = zooAnimals.filter(function(animal) {
+  return animal.population < 5;
 });
 console.log(lowPopulationAnimals);
 
